a5ea3b4f60a175b05f61f248e767cf6ff0521894
haily <49448330+melly8954@users.noreply.github.com>
2025-03-27 19:58:20 +0900

    250327 SecurityFilterChain  업데이트
    폼 로그인 등록

--- src/main/java/com/tl/securitytest/global/config/SecurityConfig.java
+++ src/main/java/com/tl/securitytest/global/config/SecurityConfig.java
@@ -12,12 +12,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((auth) -> auth.disable());
         http.
                 authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/","/login").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated());     // authenticated() 는 로그인만 진행하면 모두 접근할 수 있는 메서드      cf). denyAll() 존재
+        http.
+                formLogin((auth) -> auth
+                        .loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll());
 
         return http.build();
     }
