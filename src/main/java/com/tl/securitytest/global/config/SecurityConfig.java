30c3e4bf97406e4fba3b1f6af5fb55aa1b420cbf
haily <49448330+melly8954@users.noreply.github.com>
2025-03-27 19:17:44 +0900

    250327 SecurityConfig 추가
    SecurityFilterChain 설정

---
+++ src/main/java/com/tl/securitytest/global/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.tl.securitytest.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/","/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated());     // authenticated() 는 로그인만 진행하면 모두 접근할 수 있는 메서드      cf). denyAll() 존재
+
+        return http.build();
+    }
+}
