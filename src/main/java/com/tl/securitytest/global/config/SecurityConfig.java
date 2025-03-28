6208345f926aceafb3bc24d35aa8bdc41261534b
haily <49448330+melly8954@users.noreply.github.com>
2025-03-28 22:00:22 +0900

    250328 세션 추가 설정

--- src/main/java/com/tl/securitytest/global/config/SecurityConfig.java
+++ src/main/java/com/tl/securitytest/global/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((auth) -> auth.disable());
+                .csrf((auth) -> auth.disable());        // 배포 환경 시 csrf 공격 방지를 위해 csrf disable() 설정을 제거하고 추가적인 설정을 진행해야 한다.
         http.
                 authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/","/login","/join","/joinProc").permitAll()
@@ -26,7 +26,14 @@ public class SecurityConfig {
                         .loginPage("/login")
                         .loginProcessingUrl("/loginProc")
                         .permitAll());
-
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1)                     // 하나의 아이디에 대한 다중 로그인 허용 개수
+                        .maxSessionsPreventsLogin(true));       // 다중 로그인 개수를 초과하였을 경우 처리 방법, true : 초과시 새로운 로그인 차단 / false : 초과시 기존 세션 하나 삭제
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation().changeSessionId());  // 세션 고정 공격을 보호하기 위한 로그인 성공시 세션 설정 방법, 로그인 시 동일한 세션에 대한 id 변경
+        
         return http.build();
     }
 
